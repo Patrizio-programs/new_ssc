183592a3048d1043432acd72c5bac2a68a89dd67
PatrizioTheDev <patickmedley@gmail.com>
2025-06-16 23:26:16 -0400

    Update mainctc.js

--- ssc/src/app/components/mainctc.js
+++ ssc/src/app/components/mainctc.js
@@ -1,20 +1,17 @@
-import "./mainctc.css"
+import "./mainctc.css";
 
 export default function Mainctc() {
-
-
   return (
     <>
       <div className="hero">
         <div className="hero-content">
-          <h1 className="playfair">Dive into
-            Spanish
-            with us</h1>
+          <h1 className="playfair">Dive into Spanish with us</h1>
           <p>Engaging lessons for your immersion.</p>
-          <button type="button" className="btn">Get Started</button>
+          <button type="button" className="btn">
+            Get Started
+          </button>
         </div>
       </div>
-
     </>
-  )
+  );
 }
